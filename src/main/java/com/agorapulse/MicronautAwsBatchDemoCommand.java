fb1b400040716dd34ddc98f247d6dd06c9c12ee5
Vladimir Orany <vladimir@orany.cz>
2021-10-12 14:20:26 +0200

    using distroless, injecting bean

--- src/main/java/com/agorapulse/MicronautAwsBatchDemoCommand.java
+++ src/main/java/com/agorapulse/MicronautAwsBatchDemoCommand.java
@@ -1,28 +1,27 @@
 package com.agorapulse;
 
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
 
-import picocli.CommandLine;
+import io.micronaut.context.env.Environment;
+import jakarta.inject.Inject;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
-@Command(name = "micronaut-aws-batch-demo", description = "...",
-        mixinStandardHelpOptions = true)
+import java.time.Instant;
+
+@Command(name = "micronaut-aws-batch-demo", description = "AWS Batch Demo", mixinStandardHelpOptions = true)
 public class MicronautAwsBatchDemoCommand implements Runnable {
 
-    @Option(names = {"-v", "--verbose"}, description = "...")
-    boolean verbose;
+    @Option(names = {"-t", "--timestamp"}, description = "the time when the event was triggered")
+    Instant timestamp;
+
+    @Inject Environment environment;
 
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(MicronautAwsBatchDemoCommand.class, args);
     }
 
     public void run() {
-        // business logic here
-        if (verbose) {
-            System.out.println("Hi!");
-        }
+        System.out.printf("Event sent at %s to the environments %s", timestamp, environment.getActiveNames());
     }
 }
